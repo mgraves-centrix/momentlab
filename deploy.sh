2a4e800fff7505ab610bbe8cbcc8756b954c705f
mgraves-centrix <mattgraves@centrixlabs.com>
2026-09-03 09:26:34 -0400

    fix(deploy): enforce least privilege by defaulting ClickHouse user to momentlab_writer

--- deploy.sh
+++ deploy.sh
@@ -36,7 +36,7 @@ export CLOUDSDK_CORE_ACCOUNT="${GCP_ACCOUNT}"
 # ClickHouse & Runtime Settings (non-secret values)
 CH_HOST="${CLICKHOUSE_HOST:-nk8zetjq1w.us-east1.gcp.clickhouse.cloud}"
 CH_PORT="${CLICKHOUSE_PORT:-8443}"
-CH_USER="${CLICKHOUSE_USER:-default}"
+CH_USER="${CLICKHOUSE_USER:-momentlab_writer}"
 CH_DB="${CLICKHOUSE_DATABASE:-${CLICKHOUSE_DB:-momentlab}}"
 CH_SECURE="${CLICKHOUSE_SECURE:-true}"
 CH_WRITER_USER="${CLICKHOUSE_WRITER_USER:-momentlab_writer}"
